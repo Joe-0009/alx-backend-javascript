b45ea13ea8bfc2ff7ee1ea7a22d4a80af3f797d9
Joe-0009 <youssefrachidi1994@gmail.com>
2024-08-19 15:21:54 +0100

    retry

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,9 @@
 export default function createIteratorObject(report) {
-  let result = [];
-  for (const value of Object.values(report.allEmployees)) {
-    result = [...result, ...value];
-  }
-
-  return result;
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
 }
