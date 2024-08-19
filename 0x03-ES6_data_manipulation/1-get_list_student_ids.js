58b41692bb7f63d7b7a58d43a0f1312af4e6102b
Joe-0009 <youssefrachidi1994@gmail.com>
2024-08-19 15:24:55 +0100

    second try

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,16 @@
-export default function getListStudentIds(stud_arr) {
-  if (!Array.isArray(stud_arr)) {
-    return [];
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
-  return stud_arr.map((student) => student.id);
+  return [];
 }
--- 0x03-ES6_data_manipulation/README.md
+++ 0x03-ES6_data_manipulation/README.md
@@ -1,1 +1,1 @@
-# 0x03-ES6_data_manipulation
+readme for 0x03-ES6_data_manipulation project 
